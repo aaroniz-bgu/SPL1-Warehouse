b25414ad2b222d2e9cf1863f66d6a0762517cf97
aaroniz-bgu <aaroni@post.bgu.ac.il>
2024-01-18 22:29:00 +0200

    Docs styling

--- src/Order.cpp
+++ src/Order.cpp
@@ -14,14 +14,14 @@ Order::Order(int id, int customerId, int distance) : id(id), customerId(customer
 }
 
 /**
- * Returns the order's id
+ * @returns the order's id
  */
 int Order::getId() const {
     return id;
 }
 
 /**
- * Returns the order's customer id
+ * @returns the order's customer id
  */
 int Order::getCustomerId() const {
     return customerId;
@@ -29,6 +29,7 @@ int Order::getCustomerId() const {
 
 /**
  * Sets the order's status
+ * @param status - order status to be set
  */
 void Order::setStatus(OrderStatus status) {
     this->status = status;
@@ -36,6 +37,7 @@ void Order::setStatus(OrderStatus status) {
 
 /**
  * Sets the order's collector id
+ * @param collectorId - collector id to be set
  */
 void Order::setCollectorId(int collectorId) {
     this->collectorId = collectorId;
@@ -43,34 +45,35 @@ void Order::setCollectorId(int collectorId) {
 
 /**
  * Sets the order's driver id
+ * @param driverId - driver id to be set
  */
 void Order::setDriverId(int driverId) {
     this->driverId = driverId;
 }
 
 /**
- * Returns the order's collector id
+ * @returns the order's collector id
  */
 int Order::getCollectorId() const {
     return collectorId;
 }
 
 /**
- * Returns the order's driver id
+ * @returns the order's driver id
  */
 int Order::getDriverId() const {
     return driverId;
 }
 
 /**
- * Returns the order's status
+ * @returns the order's status
  */
 OrderStatus Order::getStatus() const {
     return status;
 }
 
 /**
- * Returns a descriptor of the order
+ * @returns a string descriptor of the order
  */
 const string Order::toString() const {
     //TODO
