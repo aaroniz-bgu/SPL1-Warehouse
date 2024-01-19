0a5f2efeee6079ca2fbcd2361c50eff534edaf41
aaroniz-bgu <aaroni@post.bgu.ac.il>
2024-01-19 14:59:47 +0200

    Initial commit

---
+++ src/WareHouse.cpp
@@ -0,0 +1,45 @@
+#include "WareHouse.h"
+
+WareHouse::WareHouse(const string &configFilePath) {
+
+}
+
+void WareHouse::start() {
+
+}
+
+void WareHouse::addOrder(Order* order) {
+
+}
+
+void WareHouse::addAction(BaseAction* action) {
+
+}
+
+Customer &WareHouse::getCustomer(int customerId) const {
+
+}
+
+Volunteer &WareHouse::getVolunteer(int volunteerId) const {
+
+}
+
+Order &WareHouse::getOrder(int orderId) const {
+
+}
+
+const vector<BaseAction*> &WareHouse::getActions() const {
+
+}
+
+void WareHouse::close() {
+
+}
+
+void WareHouse::open() {
+
+}
+
+WareHouse::~WareHouse() {
+
+}
