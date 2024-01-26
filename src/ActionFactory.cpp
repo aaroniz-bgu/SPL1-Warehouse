a0845989435615d535072824e9b6b9d8fd269ed6
sachsd@wincs.cs.bgu.ac.il <sachsd@post.bgu.ac.il>
2024-01-26 17:27:05 +0200

    Fixed bug that caused segmentation faults when entering a correct command but incorrect arguments. Or when giving an empty line.

--- src/ActionFactory.cpp
+++ src/ActionFactory.cpp
@@ -63,78 +63,83 @@ BaseAction *ActionFactory::createAction(const string &input) {
     vector<string> commands = splitIntoWords(input);
     if (!commands.empty()) {
         string type = commands[0]; // First word should be the action type
-        if (type == STEP) {
-            int numOfSteps = std::stoi(commands[1]);
-            return new SimulateStep(numOfSteps);
-        }
-        else if (type == ORDER) {
-            int orderId = std::stoi(commands[1]);
-            return new AddOrder(orderId);
-        }
-        else if (type == CUSTOMER) {
-            string customerName = commands[1];
-            string customerType = commands[2];
-            int distance = std::stoi(commands[3]);
-            int maxOrders = std::stoi(commands[4]);
-            return new AddCustomer(customerName, customerType, distance, maxOrders);
-        }
-        else if (type == ORDERSTATUS) {
-            int orderId = std::stoi(commands[1]);
-            return new PrintOrderStatus(orderId);
-        }
-        else if (type == CUSTOMERSTATUS) {
-            int customerId = std::stoi(commands[1]);
-            return new PrintCustomerStatus(customerId);
-        }
-        else if (type == VOLUNTEERSTATUS) {
-            int volunteerId = std::stoi(commands[1]);
-            return new PrintVolunteerStatus(volunteerId);
-        }
-        else if (type == LOG) {
-            return new PrintActionsLog();
-        }
-        else if (type == CLOSE) {
-            return new Close();
-        }
-        else if (type == BACKUP) {
-            return new BackupWareHouse();
-        }
-        else if (type == RESTORE) {
-            return new RestoreWareHouse();
-        }
-        else if (type == VOLUNTEER) {
-            string volunteerName = commands[1];
-            string volunteerRole = commands[2];
-            if (volunteerRole == "collector") {
-                int cooldown = std::stoi(commands[3]);
-                return new AddVolunteer(volunteerName, cooldown, NOT_LIMITED);
+        try {
+            if (type == STEP) {
+                int numOfSteps = std::stoi(commands[1]);
+                return new SimulateStep(numOfSteps);
             }
-            else if (volunteerRole == "limited_collector") {
-                int cooldown = std::stoi(commands[3]);
+            else if (type == ORDER) {
+                int orderId = std::stoi(commands[1]);
+                return new AddOrder(orderId);
+            }
+            else if (type == CUSTOMER) {
+                string customerName = commands[1];
+                string customerType = commands[2];
+                int distance = std::stoi(commands[3]);
                 int maxOrders = std::stoi(commands[4]);
-                return new AddVolunteer(volunteerName, cooldown, maxOrders);
+                return new AddCustomer(customerName, customerType, distance, maxOrders);
             }
-            else if (volunteerRole == "driver") {
-                int maxDistance = std::stoi(commands[3]);
-                int distancePerStep = std::stoi(commands[4]);
-                return new AddVolunteer(volunteerName, maxDistance, distancePerStep, NOT_LIMITED);
+            else if (type == ORDERSTATUS) {
+                int orderId = std::stoi(commands[1]);
+                return new PrintOrderStatus(orderId);
             }
-            else if (volunteerRole == "limited_driver") {
-                int maxDistance = std::stoi(commands[3]);
-                int distancePerStep = std::stoi(commands[4]);
-                int maxOrders = std::stoi(commands[5]);
-                return new AddVolunteer(volunteerName, maxDistance, distancePerStep, maxOrders);
+            else if (type == CUSTOMERSTATUS) {
+                int customerId = std::stoi(commands[1]);
+                return new PrintCustomerStatus(customerId);
+            }
+            else if (type == VOLUNTEERSTATUS) {
+                int volunteerId = std::stoi(commands[1]);
+                return new PrintVolunteerStatus(volunteerId);
+            }
+            else if (type == LOG) {
+                return new PrintActionsLog();
+            }
+            else if (type == CLOSE) {
+                return new Close();
+            }
+            else if (type == BACKUP) {
+                return new BackupWareHouse();
+            }
+            else if (type == RESTORE) {
+                return new RestoreWareHouse();
+            }
+            else if (type == VOLUNTEER) {
+                string volunteerName = commands[1];
+                string volunteerRole = commands[2];
+                if (volunteerRole == "collector") {
+                    int cooldown = std::stoi(commands[3]);
+                    return new AddVolunteer(volunteerName, cooldown, NOT_LIMITED);
+                }
+                else if (volunteerRole == "limited_collector") {
+                    int cooldown = std::stoi(commands[3]);
+                    int maxOrders = std::stoi(commands[4]);
+                    return new AddVolunteer(volunteerName, cooldown, maxOrders);
+                }
+                else if (volunteerRole == "driver") {
+                    int maxDistance = std::stoi(commands[3]);
+                    int distancePerStep = std::stoi(commands[4]);
+                    return new AddVolunteer(volunteerName, maxDistance, distancePerStep, NOT_LIMITED);
+                }
+                else if (volunteerRole == "limited_driver") {
+                    int maxDistance = std::stoi(commands[3]);
+                    int distancePerStep = std::stoi(commands[4]);
+                    int maxOrders = std::stoi(commands[5]);
+                    return new AddVolunteer(volunteerName, maxDistance, distancePerStep, maxOrders);
+                }
+                else {
+                    // Unknown volunteer role
+                }
             }
             else {
-                // Unknown volunteer role
+                std::cout << type << " is an unknown command" << std::endl;
+                return nullptr;
             }
         }
-        else {
-            std::cout << type << " is an unknown command" << std::endl;
-            return nullptr;
+        catch (exception &ex) {
+            std::cout << "Unknown command (perhaps arguments were wrong)." << std::endl;
         }
     } else {
-        // Received empty command
+        std::cout << "Received empty line, make sure you type the command correctly." << std::endl;
     }
     return nullptr;
 }
